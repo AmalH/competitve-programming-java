830295d6893d6c10a7c82579da97faa4ac2f812b
AmalH <hichri.amal@hotmail.fr>
2018-09-18 18:43:49 +0100

    initial

---
+++ HackerRankProblems/solutions/src/pragmatictheories/tech/PDFviewer.java
@@ -0,0 +1,27 @@
+package pragmatictheories.tech;
+
+import java.util.List;
+
+public class PDFViewer {
+
+    // Complete the designerPdfViewer function below.H
+    static int designerPdfViewer(int[] h, String word) {
+
+        int height=1;
+
+        String[] alphabets=new  String[]{"a", "b", "c","d","e", "f", "g","h","i", "j", "k","l","m", "n",   "o","p","q", "r", "s","t","u", "v", "w","x","y", "z"};
+
+        HashMap<String,Integer> heights = new HashMap();
+
+        for (int i=0;i<h.length;i++)
+            heights.put(alphabets[i],h[i]);
+
+        for(int i=0;i<word.length();i++)
+            if(heights.get(Character.toString(word.charAt(i)))>height)
+                height=heights.get(Character.toString(word.charAt(i)));
+
+        return height*word.length();
+    }
+
+
+}
