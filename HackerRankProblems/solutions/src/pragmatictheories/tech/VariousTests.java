ddd675801f24c700456a315f22f2c40f44bb8a5d
AmalH <hichri.amal@hotmail.fr>
2018-11-01 12:43:34 +0100

    strings

--- HackerRankProblems/solutions/src/pragmatictheories/tech/VariousTests.java
+++ HackerRankProblems/solutions/src/pragmatictheories/tech/VariousTests.java
@@ -11,22 +11,35 @@ public class VariousTests {
 
     /**
      * if a string has all unique characters
-     * bad implementation, this is a 2*/
+     * bad implementation, this is a O(n2) time solution
+     */
     private static boolean hasAllUniqueChars(String s){
 
-        boolean res=true;
-
-        outerloop:
         for(int i=1;i<s.length();i++){
             for(int j=i+1;j<s.length();j++){
                 if(s.charAt(i)==s.charAt(j)){
-                    res = false;
-                    break outerloop;
+                    return false;
                 }
             }
         }
 
-        return res;
+        return true;
     }
+    /** an O(n) solution could be **/
+    public boolean isUniqueChars2(String str){
+        if(str.length()>256) return false;
+        boolean[] char_set = new boolean[256];
+        for(int i=0;i<str.length();i++){
+            int val = str.charAt(i);
+            if(char_set[val])
+                return false;
+            char_set[val]=true;
+        }
+        return true;
+    }
+
+    /**
+     * replace all spaces in a string with '%20'
+     */
 
 }
